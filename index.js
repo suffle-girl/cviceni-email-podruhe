8ad48cee8c0f3812567b20d1827072bb1b23d65e
Šárka Flídrová <sarka.flidrova@email.cz>
2024-04-06 11:34:53 +0200

    Add functionality as per instructions.

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const goodbye = (name) => {
-  return 'S pozdravem ' + name;
+  return "S pozdravem " + name;
+};
+
+const formalGoodbye = (name) => {
+  return `S uctivou poklonou ${name}`;
+};
+
+const rudeGoodbye = (name) => {
+  return `Se měj, ${name}`;
 };
 
 const fillSubject = (subject) => {
-  document.querySelector('.email__subject').textContent = subject;
+  document.querySelector(".email__subject").textContent = subject;
 };
 
-const fillBody = (body, name) => {
-  const bodyElm = document.querySelector('.email__body');
+const fillBody = (body, name, goodbyeFunction) => {
+  const bodyElm = document.querySelector(".email__body");
   bodyElm.innerHTML += body;
-  const closingElm = document.querySelector('.email__closing');
-  closingElm.textContent = goodbye(name);
+  const closingElm = document.querySelector(".email__closing");
+  closingElm.textContent = goodbyeFunction(name);
 };
+
+fillSubject("Obchodní sdělení");
+fillBody(
+  "Kupte mycí prostředek, který vám vytře zrak, se slevou 50 %.",
+  "Jan Čistý",
+  formalGoodbye
+);
+
+fillSubject("Pozvánka na oslavu narozenin");
+fillBody(
+  "Zítra oslava. 18:00 ve Starý hospodě.", 
+  "Patrik Veselý", 
+  rudeGoodbye
+  );
